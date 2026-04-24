016ce080740d74f3016e844e2e8f992aa43c5e5a
Samara Bolaños González <samarabgonzalez@gmail.com>
2026-04-24 08:35:46 +0100

    Extraer métodos y dejar main limpio

--- .idea/AñosPrimos.java
+++ .idea/AñosPrimos.java
@@ -2,6 +2,24 @@ public class Mainb {
     public static void main(String[] args) {
         int añoPrimo = 0;
         int año = 2008;
+        añoPrimo = getAñoPrimo(año, añoPrimo);
+        esParImpar(añoPrimo);
+    }
+
+    private static void esParImpar(int añoPrimo) {
+        System.out.println("El primer año primo encontrado es " + añoPrimo);
+        if (añoPrimo % 2 != 0) {
+            System.out.println("Que es un año par");
+        } else
+        if (añoPrimo > 2008) {
+            if (añoPrimo < 2033) {
+                System.out.println("y está comprendido entre 2008 y 2033");
+            }
+        }
+        return;
+    }
+
+    private static int getAñoPrimo(int año, int añoPrimo) {
         while (año <= 2033) {
             if (añoPrimo == 0) {
                 boolean esPrimo = true;
@@ -17,15 +35,6 @@ public class Mainb {
             año++;
             continue;
         }
-        System.out.println("El primer año primo encontrado es " + añoPrimo);
-        if (añoPrimo % 2 != 0) {
-            System.out.println("Que es un año par");
-        } else
-        if (añoPrimo > 2008) {
-            if (añoPrimo < 2033) {
-                System.out.println("y está comprendido entre 2008 y 2033");
-            }
-        }
-        return;
+        return añoPrimo;
     }
 }
